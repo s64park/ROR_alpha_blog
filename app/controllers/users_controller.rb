dd5bbc5cb46f554f8036a0c47f6759163e1658a6
s64park <danny.park@overbond.com>
2017-02-21 01:05:54 -0500

    fixed typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      seesion[:user_id] = @user.id
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
       redirect_to user_path(@user)
     else
